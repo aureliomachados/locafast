a02ef56800ebd0dc573f05a80136fea9405d1245
Aurélio Guedes <aureliomachados@gmail.com>
2016-11-11 12:23:02 -0200

    Changes to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        //$this->call(PaisesTableSeeder::class);
-        //$this->call(EstadosTableSeeder::class);
-        //$this->call(CidadesTableSeeder::class);
+        $this->call(PaisesTableSeeder::class);
+        $this->call(EstadosTableSeeder::class);
+        $this->call(CidadesTableSeeder::class);
     }
 }
